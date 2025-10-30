b5cc148f595bc18dd63b352bdbe2e297290de007
Arthurzão <arthborgestv3333@gmail.com>
2025-10-30 09:14:55 -0300

    Refactor pedido.c for safer input handling
    Updated input methods to use fgets for safety and added header check for CSV file.

--- ModuloPedido/pedido.c
+++ ModuloPedido/pedido.c
@@ -6,8 +6,6 @@ typedef struct{
      char numero_pedido[50];
      char nome_cliente[100];
      char produto_pedido[100];
-    
-     
 } cadastro;
 
 int main(){
@@ -16,20 +14,31 @@ int main(){
     FILE *fp;
 
     fp = fopen("pedido.csv", "a+");
-
-    printf("Digite o Pedido que deseja cadastrar:");
-    gets(c.numero_pedido, 50, stdin);
-
-    printf("Nome do cliente:");
-    gets(c.nome_cliente, 100, stdin);
-
-    printf("Produto solicitado:");
-    gets(c.produto_pedido, 100, stdin);
-
+    
+    rewind(fp);
+    char primeira_linha[100];
+    if (fgets(primeira_linha, 100, fp) == NULL) {
+        fprintf(fp, "ID, CLIENTE, PRODUTO\n");
+    };
+    
+    printf("Digite o Pedido que deseja cadastrar:\n");
+    fgets(c.numero_pedido, 50, stdin);
+    c.numero_pedido[strcspn(c.numero_pedido, "\n")] = '\0';
+    
+    printf("\nNome do cliente:\n");
+    fgets(c.nome_cliente, 100, stdin);
+    c.nome_cliente[strcspn(c.nome_cliente, "\n")] = '\0';
+    
+    printf("\nProduto solicitado:\n");
+    fgets(c.produto_pedido, 100, stdin);
+    c.produto_pedido[strcspn(c.produto_pedido, "\n")] = '\0';
+    
     fprintf(fp, "%s, %s,  %s\n", c.numero_pedido, c.nome_cliente, c.produto_pedido);
 
+    fclose(fp);
     
-
+    
+    return 0;
 
 }
 
