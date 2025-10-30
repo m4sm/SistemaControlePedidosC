028115d2d77cd45a87942dc4d46f50fe2356a1ca
Rodrigo Almeida <rodrigo.almeida@aluno.ueg.br>
2025-10-29 21:08:09 -0300

    Create funcoes.h
    Cabeçarios de funções

---
+++ Modulo Produto/funcoes.h
@@ -0,0 +1,12 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include "structs.h"
+
+// Verifica se o arquivo existe e cria se necessário
+void verificarOuCriarArquivo( const char *nomeArquivo, const char *tipo);
+
+
+
+
+#endif
