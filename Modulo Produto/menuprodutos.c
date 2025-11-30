3ce1aa83f0b8b980bc5eec70a0d1eb9dc13da282
Rodrigo Almeida <rodrigo.almeida@aluno.ueg.br>
2025-11-30 00:04:08 -0300

    Update menuprodutos.c
    Adaptação para o ncurses

--- Modulo Produto/menuprodutos.c
+++ Modulo Produto/menuprodutos.c
@@ -1,38 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>    
-#include "produtos.h"
-#include "produtos.c"
+#include <stdlib.h>
+#include <ncurses.h>
+#include "produtos.h" /
 
+int menuProdutos() { 
+    // Inicia o ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
 
-void mainProdutos() { //renomear para menuProdutos() quando se integrar na função principal.
-    Produto p;
     int opcao;
-   
+
     do {
-        printf("\n=== Sistema de Produtos ===\n");
-        printf("1. Cadastrar Produto\n");
-        printf("2. Consultar Produto\n");
-        printf("3. Remover Produto\n");
-        printf("4. Listar Produtos\n");
-        printf("0. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-        getchar();
-
-
+        clear();
+        printw("=== Sistema de Produtos ===\n");
+        printw("1. Cadastrar Produto\n");
+        printw("2. Consultar Produto\n");
+        printw("3. Remover Produto\n");
+        printw("4. Listar Produtos\n");
+        printw("0. Sair\n");
+        printw("Escolha uma opcao: ");
+        refresh();
+        
+        scanw("%d", &opcao);
+        
+        // switch-case
         switch (opcao) {
-            case 1: {
+            case 1: 
                 do {
+                    clear();
                     cadastrarProduto("listadeprodutos.csv");
                 } while (desejaContinuar());
                 break;
-                }
-            case 2: consultarProduto("listadeprodutos.csv"); break;
-            case 3: removerProduto("listadeprodutos.csv"); break;
-            case 4: listarProdutos("listadeprodutos.csv"); break;
-            case 0: printf("Saindo...\n"); break;
-            default: printf("Opcao invalida!\n");
+                
+            case 2: 
+                consultarProduto("listadeprodutos.csv"); 
+                break;
+            case 3: 
+                removerProduto("listadeprodutos.csv"); 
+                break;
+            case 4: 
+                listarProdutos("listadeprodutos.csv"); 
+                break;
+            case 0: 
+                printw("Saindo...\n"); 
+                break;
+            default: 
+                printw("Opcao invalida!\n");
+                getch();
         }
     } while (opcao != 0);
 
+    endwin(); // Encerra ncurse
+    return 0;
 }
